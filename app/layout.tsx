963dcedd404d37e98a6a0f7a54f54707dc1805f1
LIN800419 <s255031@gmail.com>
2026-03-31 14:29:46 +0800

    update app page

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,11 +7,11 @@ export const metadata: Metadata = {
   description: '個人紀錄工具',
   icons: {
     icon: [
-      { url: '/icon-192x192.png', sizes: '192x192', type: 'image/png' },
-      { url: '/icon-512x512.png', sizes: '512x512', type: 'image/png' },
+      { url: '/icons/icon-192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/icons/icon-512.png', sizes: '512x512', type: 'image/png' },
     ],
-    apple: '/apple-touch-icon.png',
-    shortcut: '/icon-192x192.png',
+    apple: '/icons/apple-touch-icon.png',
+    shortcut: '/icons/icon-192.png',
   },
 }
 
--- app/manifest.ts
+++ app/manifest.ts
@@ -11,17 +11,23 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#111111',
     icons: [
       {
-        src: '/icon-192x192.png',
+        src: '/icons/icon-192.png',
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        src: '/icon-512x512.png',
+        src: '/icons/icon-512.png',
         sizes: '512x512',
         type: 'image/png',
       },
       {
-        src: '/apple-touch-icon.png',
+        src: '/icons/icon-512-maskable.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: '/icons/apple-touch-icon.png',
         sizes: '180x180',
         type: 'image/png',
       },
